2cc447a3024f86abfb4b502245fffb9b6e57ad09
Alex666 <restrepoagudeloalexander@gmail.com>
2023-05-17 20:49:32 -0500

    crea varias listas y muestra su tamaño, crea el .gitignore

---
+++ main.py
@@ -0,0 +1,24 @@
+
+edad = 20
+altura= 1.84
+nombre= "Alex"
+estado= True
+
+lista1=[10,5,6,9]
+lista2=[1.84,1.70,1.55,89.4]
+lista3=["lunes","martes","miercoles"]
+lista4=["alex",80,1.84,False]
+
+
+if __name__ == '__main__':
+
+    print(len(lista1))
+    print(len(lista2))
+    print(len(lista3))
+    print(len(lista4))
+
+    print()
+    print(lista1)
+    lista1[0]=1
+    print()
+    print(lista1)
